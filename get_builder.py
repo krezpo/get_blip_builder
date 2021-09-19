682cb41500faf149014d2691ec6f4ea99f8642ca
Eduardo Germano <krezpo@gmail.com>
2021-09-19 15:58:00 -0300

    fixing yaml and adding requirements

--- get_builder.py
+++ get_builder.py
@@ -4,8 +4,6 @@ import requests
 import uuid
 import json
 
-URL = "<URL_COMMANDS>"
-
 if __name__ == "__main__":
 
     with open("config.yml") as yaml_file:
@@ -14,29 +12,29 @@ if __name__ == "__main__":
     if config == None:
         sys.exit(-1)
     
-    for bot in config:
-        #print(bot, config[bot])
+    chatbots = config["chatbots"]
+    for chatbot in chatbots:
 
         headers = {'Content-Type' : 'application/json',
-                   'Authorization': config[bot]['auth']}
+                   'Authorization': chatbots[chatbot]['auth']}
 
         payload = {"id": str(uuid.uuid1()),
                    "method": "get",
                    "uri": "/buckets/blip_portal:builder_published_flow"}
         
-        print("Pegando fluxo em PRD do {}".format(config[bot]['name']))
-        r = requests.post(URL, headers=headers, data=json.dumps(payload), timeout=100)
+        print("Pegando fluxo em PRD do {}".format(chatbots[chatbot]['name']))
+        r = requests.post(config["commands_url"], headers=headers, data=json.dumps(payload), timeout=100)
 
         if r.status_code != 200:
-            error = {'error': 'take connection refused for get status {}'.format(config[bot]['name'])}
+            error = {'error': 'take connection refused for get status {}'.format(chatbots[chatbot]['name'])}
             print(error)
             sys.exit(r.status_code)
         else:
             resource = r.json()["resource"]
 
-            print("Salvando fluxo do {} em {}.json".format(config[bot]['name'], bot))
+            print("Salvando fluxo do {} em {}.json".format(chatbots[chatbot]['name'], chatbot))
 
-            with open("..\\builder\{}.json".format(bot), 'w') as outfile:
+            with open("{}/{}.json".format(config["destionation_folder"], chatbot), 'w') as outfile:
                 json.dump(resource, outfile, indent=4)
 
     print("Fim de execução")
